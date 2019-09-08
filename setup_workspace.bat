46bd62eb49cd48da5ffeb7b740fa5fd5bed16354
Alex Finney <alexander.n.finney@gmail.com>
2019-09-08 09:45:59 -0500

    Build updates

--- setup_workspace.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-rd /s /q "run/mods"
-mkdir "run/mods"
-REM copy "external_deps\worldedit-forge-mc1.12-6.1.8-dist.jar" "run\mods\worldedit-forge-mc1.12-6.1.8-dist.jar"
-
-gradlew setupDecompWorkspace
-
